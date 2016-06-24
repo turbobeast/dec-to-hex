bd6348f7aa5a5481e941b9641c106758029983c6
turbobeast <georgemichaelfaust@gmail.com>
2016-06-23 22:27:48 -0400

    v2.0

--- test/index.js
+++ test/index.js
@@ -3,9 +3,9 @@ const test = require('tape')
 const decToHex = require('../src/index')
 
 test('dec-to-hex', t => {
-  t.plan(200000);
+  t.plan(20000);
 
-  for (let i = 0; i < 200000; i += 1) {
+  for (let i = 0; i < 20000; i += 1) {
     t.equal(decToHex(i), (i).toString(16))
   }
 })
